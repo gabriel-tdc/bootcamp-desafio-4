fe4f70e779d22409a1909559eb44de1331f1aa3c
Gabriel Tedeschi <gabriel-tdc@hotmail.com>
2020-03-15 10:37:54 -0300

    Ajustes para responsivo

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,7 +7,34 @@ class PostList extends Component {
     state = {
         posts: [
             {
-              id: 1,
+              id: 3,
+              author: {
+                name: "Rafael Tedeschi",
+                avatar: "https://avatars2.githubusercontent.com/u/16822843?s=460&v=4"
+              },
+              date: "15 Fev 2020",
+              content: "Quais são as linguagens que vocês tem mais experiência?",
+              comments: [
+                {
+                  id: 1,
+                  author: {
+                    name: "Gabriel Tedeschi",
+                    avatar: "https://avatars2.githubusercontent.com/u/12170214?s=460&u=310bf8437112e32b7d7023d74b8c1c9917e21965&v=4"
+                  },
+                  content: "Eu comecei com PHP, e agora estou migrando para NodeJS e ReactJS, junto do ReactNative!"
+                },
+                {
+                  id: 2,
+                  author: {
+                    name: "Rafael Tedeschi",
+                    avatar: "https://avatars2.githubusercontent.com/u/16822843?s=460&v=4"
+                  },
+                  content: "Eu conheço um pouco de PHP, mas meu forte mesmo é JAVA... Comecei o NodeJS agora também"
+                }
+              ]
+            },
+            {
+              id: 2,
               author: {
                 name: "Gabriel Tedeschi",
                 avatar: "https://avatars2.githubusercontent.com/u/12170214?s=460&u=310bf8437112e32b7d7023d74b8c1c9917e21965&v=4"
@@ -29,12 +56,20 @@ class PostList extends Component {
                     name: "Diego Fernandes",
                     avatar: "https://avatars1.githubusercontent.com/u/2254731?s=460&u=dc1a4fd280cdc3c6977bacf57cbfeb8ba0917f27&v=4"
                   },
-                  content: "Cara, você já viu o TDD?"
+                  content: "Cara, você já viu o ReactNative?"
+                },
+                {
+                  id: 3,
+                  author: {
+                    name: "Rafael Tedeschi",
+                    avatar: "https://avatars2.githubusercontent.com/u/16822843?s=460&v=4"
+                  },
+                  content: "Ahh.. tem o Springboot também :)"
                 },
               ]
             },
             {
-              id: 2,
+              id: 1,
               author: {
                 name: "Gabriel Tedeschi",
                 avatar: "https://avatars2.githubusercontent.com/u/12170214?s=460&u=310bf8437112e32b7d7023d74b8c1c9917e21965&v=4"
@@ -49,11 +84,11 @@ class PostList extends Component {
 
     render() {
         return(
-            <>
+            <div className="content">
                 {this.state.posts.map(
                     post => <Post key={post.id} data={post} />
                 )}
-            </>
+            </div>
         );
     }
 }
